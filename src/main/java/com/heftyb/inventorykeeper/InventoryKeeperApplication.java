288aa1d3fb077404003f03f5ad012c20ef5e13c5
Andrew Shields <35949416+HeftyB@users.noreply.github.com>
2023-03-04 20:46:55 -0500

    Initial commit

---
+++ src/main/java/com/heftyb/inventorykeeper/InventoryKeeperApplication.java
@@ -0,0 +1,13 @@
+package com.heftyb.inventorykeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryKeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryKeeperApplication.class, args);
+    }
+
+}
